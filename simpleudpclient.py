e3d8590924752cd210787d93903b81baf04b5416
Megala <40885329+megusesh@users.noreply.github.com>
2018-07-06 14:24:23 +0530

    Create simpleudpclient.py

---
+++ simpleudpclient.py
@@ -0,0 +1,8 @@
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NO=6789
+Message="welcome,server"
+bytesToSend=str.encode(Message)
+clientSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+clientSock.sendto(bytesToSend,(UDP_IP_ADDRESS,UDP_PORT_NO))
+                                             
