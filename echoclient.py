ce129064c66fca6f0e0def2263e1bbb6c18dbb2d
Megala <40885329+megusesh@users.noreply.github.com>
2018-07-13 13:34:56 +0530

    Create echoclient.py

---
+++ echoclient.py
@@ -0,0 +1,10 @@
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NO=6789
+Message="welcome,server"
+bytesToSend=str.encode(Message)
+clientSock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+clientSock.sendto(bytesToSend,(UDP_IP_ADDRESS,UDP_PORT_NO))
+data=clientSock.recvfrom(1024)
+print(data)
+ 
